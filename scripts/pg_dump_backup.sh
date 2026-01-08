56d75dd2082aa4f785d316052bea08c3f59b9042
Sugandha <80295447+DevOpsJen@users.noreply.github.com>
2026-01-08 23:38:51 +0530

    Update pg_dump_backup.sh
    Add date → no overwrite
    
    Upload to S3 → safe storage
    
    Print message → confirmation

--- scripts/pg_dump_backup.sh
+++ scripts/pg_dump_backup.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+
+# Variables
 RDS_ENDPOINT="<YOUR_RDS_ENDPOINT>"
 DB_USER="<YOUR_DB_USERNAME>"
 DB_NAME="<YOUR_DB_NAME>"
+S3_BUCKET="<YOUR_S3_BUCKET_NAME>"
 
-pg_dump -Fc -h "$RDS_ENDPOINT" -U "$DB_USER" "$DB_NAME" > backup.dump
+# Timestamp
+DATE=$(date +%F-%H-%M)
+
+# Backup file name
+BACKUP_FILE="${DB_NAME}_${DATE}.dump"
+
+# Create backup
+pg_dump -Fc -h "$RDS_ENDPOINT" -U "$DB_USER" "$DB_NAME" > "$BACKUP_FILE"
+
+# Upload to S3
+aws s3 cp "$BACKUP_FILE" s3://$S3_BUCKET/$BACKUP_FILE
+
+echo "Backup completed and uploaded to S3"
